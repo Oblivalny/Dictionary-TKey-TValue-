b95f4c4d47a6d47b8565c78caeaf7f1904dbef8e
днс <@DESKTOP-P6D9CRD>
2017-05-17 21:39:51 +0500

    Добавьте файлы проекта.

---
+++ Slovari____/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slovari____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"C:\Users\днс\Desktop\text.txt");
+
+            string unic = "";
+            text.ToLower();
+            int count = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < unic.Length; j++)
+                {
+                    if (text[i]==unic[j])
+                    {
+                        count++;
+                    }
+                }
+
+                if (count == 0)
+                {
+                    unic = unic + text[i];
+                }
+
+            }
+
+
+            Dictionary<string, int> slovari = new Dictionary<string, int>();
+
+            for (int i = 0; i < unic.Length; i++)
+            {
+                slovari.Add(Convert.ToString(unic[i]), 0);
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                slovari[Convert.ToString(text[i])]++;
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
